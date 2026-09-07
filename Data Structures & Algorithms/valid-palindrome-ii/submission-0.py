41e99b0e8576fdbb0ecfa27e8938ef1da1782057
Jones Peprah Nsiah <jnsiah@gsumail.gram.edu>
2026-09-07 02:36:19 -0500

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def palindrome(l, r):
+            while l<r:
+                if s[l]!=s[r]:
+                    return False
+                l+=1
+                r-=1
+
+            return True
+
+        l,r=0,len(s)-1
+        while l<r:
+            if s[l]!=s[r]:
+                return (palindrome(l,r-1) or
+                palindrome(l+1,r))
+            l+=1
+            r-=1
+        return True
+    
+        
